a581771169f7915d5c8b033fa278ce770b1697ce
lingtaoxue <divecoder@gmail.com>
2022-01-04 16:20:44 +0800

    comment off unavailable scene

--- extra-engine/vk_engine.cpp
+++ extra-engine/vk_engine.cpp
@@ -1612,8 +1612,9 @@ void VulkanEngine::init_scene()
 	
 	glm::mat4 unrealFixRotation = glm::rotate(glm::radians(-90.f), glm::vec3{ 1,0,0 });
 	
-	load_prefab(asset_path("Sponza2.pfb").c_str(), sponzaMatrix);
-	load_prefab(asset_path("scifi/TopDownScifi.pfb").c_str(),  glm::translate(glm::vec3{0,20,0}));
+	//load_prefab(asset_path("Sponza/Sponza.pfb").c_str(), sponzaMatrix);
+	//load_prefab(asset_path("Sponza2.pfb").c_str(), sponzaMatrix);
+	//load_prefab(asset_path("scifi/TopDownScifi.pfb").c_str(),  glm::translate(glm::vec3{0,20,0}));
 	int dimcities = 2;
 	for (int x = -dimcities; x <= dimcities; x++) {
 		for (int y = -dimcities; y <= dimcities; y++) {
@@ -1625,7 +1626,7 @@ void VulkanEngine::init_scene()
 			glm::mat4 cityMatrix = translation;// * glm::scale(glm::mat4{ 1.0f }, glm::vec3(.01f));
 			//load_prefab(asset_path("scifi/TopDownScifi.pfb").c_str(), unrealFixRotation * glm::scale(glm::mat4{ 1.0 }, glm::vec3(.01)));
 			//load_prefab(asset_path("PolyCity/PolyCity.pfb").c_str(), cityMatrix);
-			load_prefab(asset_path("CITY/polycity.pfb").c_str(), cityMatrix);
+			//load_prefab(asset_path("CITY/polycity.pfb").c_str(), cityMatrix);
 		//	load_prefab(asset_path("scifi/TopDownScifi.pfb").c_str(), cityMatrix);
 		}
 	}
